0c0d837997a5cbde7287678efcf7d9c76fe8fa70
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-20 18:50:24 +0800

    solve problem by comparing each characters equivalent value to the object, if the current value is smaller than the next value, diminish its value to the accumulator, if not add

--- LC-13-Roman-to-Integer.js
+++ LC-13-Roman-to-Integer.js
@@ -33,3 +33,76 @@ Example 1:
     Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 */
+
+//Solution 1
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function (s) {
+    symbols = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    let accumulator = 0;
+    for (let i = 0; i < s.length; i++) {
+        symbols[s[i]] < symbols[s[i + 1]]
+            ? (accumulator -= symbols[s[i]])
+            : (accumulator += symbols[s[i]]);
+    }
+    return accumulator;
+};
+
+//Solution 2
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    s = Array.from(s);
+    let tempArr = [];
+    let accumulator = 0;
+    s.forEach((value) => {
+        console.log(value);
+        switch (value) {
+            case 'I':
+                tempArr.push(1);
+                break;
+            case 'V':
+                tempArr.push(5);
+                break;
+            case 'X':
+                tempArr.push(10);
+                break;
+            case 'L':
+                tempArr.push(50);
+                break;
+            case 'C':
+                tempArr.push(100);
+                break;
+            case 'D':
+                tempArr.push(500);
+                break;
+            case 'M':
+                tempArr.push(1000);
+                break;
+        }
+    });
+
+    for (let i = 0; i < tempArr.length; i++) {
+        if (tempArr[i] < tempArr[i + 1]) {
+            accumulator += tempArr[i + 1] - tempArr[i];
+            i++;
+        } else {
+            accumulator += tempArr[i];
+        }
+    }
+
+    return accumulator;
+};
