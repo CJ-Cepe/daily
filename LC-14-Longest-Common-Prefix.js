6a78425168451d692dc5fc9964a0090abc4ee0a2
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-21 18:39:51 +0800

    solve problem by sorting, comparing each character of the first and last string, and return the slice of the word till the index

--- LC-14-Longest-Common-Prefix.js
+++ LC-14-Longest-Common-Prefix.js
@@ -8,4 +8,24 @@ Example 1
     Input: strs = ["flower","flow","flight"]
     Output: "fl"
 
+Solution
+    - sort string
+    - compare only the first and the last, with the first being the main basis
+    - compare per char, if a char is not equal, return slice of first string till index
+    - return first string as main fall/catcher
 */
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+var longestCommonPrefix = function (strs) {
+    strs.sort();
+    for (let i = 0; i < strs[0].length; i++) {
+        if (strs[0][i] !== strs[strs.length - 1][i]) {
+            return strs[0].substring(0, i);
+        }
+    }
+    return strs[0];
+};
