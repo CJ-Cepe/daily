68a7b074b706dc4980212cbc6b8d0aa33c4d1fad
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-22 17:36:51 +0800

    solve problem using stack

--- LC-20-Valid-Parentheses.js
+++ LC-20-Valid-Parentheses.js
@@ -1,13 +1,49 @@
 /*
+20. Valid Parentheses
 
-Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+determine if the input string is valid.
 
 An input string is valid if:
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
 
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Every close bracket has a corresponding open bracket of the same type.
-
-
+Solution
+    1. read per character
+    2. initialize an empty stack
+    3. if (, [, { is encounter, push their counter part symbol in the stack
+    4. else if other is encountered, pop the stack and compare to the current character
+    5. if the same, continue. if not return false
+    6. after traversing, if array is empty return true else false
 
 */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    //Solution 4
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        switch (s[i]) {
+            case '(':
+                stack.push(')');
+                break;
+            case '{':
+                stack.push('}');
+                break;
+            case '[':
+                stack.push(']');
+                break;
+            default:
+                if (stack.pop() !== s[i]) {
+                    //pop()
+                    return false;
+                }
+        }
+    }
+
+    return !stack.length;
+};
