a348d664a8cfce980936108f98747ddca2b6c541
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-24 21:40:32 +0800

    solve problem

--- LC-27-Remove-Element.js
+++ LC-27-Remove-Element.js
@@ -25,4 +25,44 @@ Example
         with the first five elements of nums containing 0, 0, 1, 3, and 4.
     Note that the five elements can be returned in any order.
     It does not matter what you leave beyond the returned k (hence they are underscores).
+    
+Solution 1
+    1. traverse through the whole array
+    2. if current element is equals to the target
+    3.      remove element - using splice()
+    4.      i--
+    5. return current array lenght
+
+Solution 2
+    1. set another pointer to 0
+    2. traverse through the whole array - for loop, j iterator
+    3. if current element is NOT equals to the target
+    4.      array[i] = array[j]
+    5.      i++
+    6. return i
 */
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    //solution 1
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+
+    //solution 2
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i++] = nums[j];
+        }
+    }
+    return i;
+};
