8181fb5a9c419a0998ecef03e0b38bf9221b5442
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-28 13:02:16 +0800

    add solution details

--- LC-58-Length-of-Last-Word.js
+++ LC-58-Length-of-Last-Word.js
@@ -15,4 +15,21 @@ Example 2:
     Input: s = "   fly me   to   the moon  "
     Output: 4
     Explanation: The last word is "moon" with length 4.
+
+Solution 1 - loop through end
+    1. trimEnd of the string
+    2. check string if empty, return 0
+    3. loop backwards, find the index of the last " "
+    4. return string length - index of " "
+    5. outside of loop, return string length, to handle string without spaces
+
+Solution 2 - lastIndexOf
+    1. trim string
+    2. return string length - 1 - lastIndexOf(" ")
+
+Solution 3 - split and pop
+    1. trim string
+    2. split string " "
+    3. pop last element
+    4. return length of last element
 */
