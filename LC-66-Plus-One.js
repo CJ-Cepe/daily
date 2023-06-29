c8207dcfabb00f46696423b153e2ee5e2f5c951b
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-29 14:06:17 +0800

    solve problem

--- LC-66-Plus-One.js
+++ LC-66-Plus-One.js
@@ -34,9 +34,42 @@ Example 3:
     Incrementing by one gives 9 + 1 = 10.
     Thus, the result should be [1,0].
 
+Solution 1 - hardcode
+    1. traverse through the whole array in reverse
+    2. With for loop, if current element is < 9
+    3.      element + 1 then return array
+    4. else, element = 0 then continue loop
+    5. if loop finishes, unshift 1
+    6. return array
 
-
-
-
+Solution 2 - bigint and string
+    1. join array and cast as bigInt 
+    2. add +
+    3. convert to string
+    4. split to turn string to array
+    5. cast each element as number
+    6. return array
 
 */
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+    //solution 1
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
+
+    //solution 2
+    let num = BigInt(digits.join(''));
+    digits = String(++num).split('').map(Number);
+    return digits;
+};
