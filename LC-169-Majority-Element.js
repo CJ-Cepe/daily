168968cb0da5c18c9008e2956f42805ffd374eee
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-07-11 19:08:48 +0800

    add solution 2

--- LC-169-Majority-Element.js
+++ LC-169-Majority-Element.js
@@ -14,7 +14,7 @@ Example 2:
     Input: nums = [2,2,1,1,1,2,2]
     Output: 2
 
-Solution 1
+Solution 1 - look-up objects
     1. initialize an empty object, result = 0, and maxCount = 0
     2. traverse through the whole array to count each elements' instances
     3. per iteration, append equivalent key in the object
@@ -22,6 +22,14 @@ Solution 1
     5.      maxCount = current Property
     6.      res = current element
     7. return result
+
+Solution 2 - stack
+    1. initialize an empty stack
+    2. traverse through the whole array
+    3. if current elem is same with the values in the stack, push elem
+    4. else pop elem
+    5. if stack is empty, push new elem
+    6. return the 1st elem of the stack
 */
 
 /**
@@ -29,7 +37,7 @@ Solution 1
  * @return {number}
  */
 var majorityElement = function (nums) {
-    //Solution 1 - objects
+    //Solution 1 - look-up objects
     let numInstances = {},
         res = 0,
         maxCount = 0;
@@ -41,4 +49,17 @@ var majorityElement = function (nums) {
         }
     }
     return res;
+
+    //Solution 2 - stack
+    let maxElem = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (!maxElem.length) {
+            maxElem.push(nums[i]);
+        } else if (maxElem[0] == nums[i]) {
+            maxElem.push(nums[i]);
+        } else {
+            maxElem.pop();
+        }
+    }
+    return maxElem[0];
 };
