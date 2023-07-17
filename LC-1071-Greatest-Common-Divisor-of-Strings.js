e201a9cf091a5899067ee2ec69ba576972d7ebf8
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-07-17 23:55:53 +0800

    add solution 1

--- LC-1071-Greatest-Common-Divisor-of-Strings.js
+++ LC-1071-Greatest-Common-Divisor-of-Strings.js
@@ -19,4 +19,44 @@ Example 2:
 Example 3:
     Input: str1 = "LEET", str2 = "CODE"
     Output: ""
+
+Solution 1
+
 */
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+    //Clue
+    // is concatenated with itself one or more times
+    // absolute copy of it self
+    // has complete factor of it
+
+    //Solution 1
+    //1. get min length
+    //2. traverse through the string till 0 - reverse
+    //3. if divisible to both string
+
+    let length = Math.min(str1.length, str2.length);
+    for (let i = length; i >= 0; i--) {
+        let chunk = str1.slice(0, i);
+        if (
+            str1.length % chunk.length === 0 &&
+            str2.length % chunk.length === 0
+        ) {
+            let tempValue1 = chunk.repeat(
+                Math.floor(str1.length / chunk.length)
+            );
+            let tempValue2 = chunk.repeat(
+                Math.floor(str2.length / chunk.length)
+            );
+            if ((tempValue1 == str1) & (tempValue2 == str2)) {
+                return str1.slice(0, i);
+            }
+        }
+    }
+    return '';
+};
