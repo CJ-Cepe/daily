3fac879b8612b721e36b929dec1d226a05962af1
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-07-26 23:19:53 +0800

    add solution 1

--- LC-217-Contains-Duplicate.js
+++ LC-217-Contains-Duplicate.js
@@ -16,4 +16,39 @@ Example 2:
 Example 3:
     Input: nums = [1,1,1,3,3,4,3,2,4,2]
     Output: true
+
+Solution 1
+    1. sort array
+    2. use counter
+
+
 */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    //Solution 1 - sort -> traverse -> counter
+    /*
+    nums = nums.sort()
+
+    let index = 1,
+        counter = 1,
+        val = nums[0]
+
+    while(index<nums.length && counter < 2){
+        if(nums[index]==val){
+            counter++
+        }else {
+            val = nums[index]
+            counter = 1
+        }
+        index++
+    }
+
+    return counter >= 2
+    */
+    //solution 3 - using objects
+    //Solution 4 - using sets
+};
