dfc0af1d50357468a504063afa2aa62cbdedd154
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-07-29 00:46:43 +0800

    add solution 1

--- LC-219-Contains-Duplicate-II.js
+++ LC-219-Contains-Duplicate-II.js
@@ -13,4 +13,24 @@ Example 1:
 Example 2:
     Input: nums = [1,0,1,1], k = 1
     Output: true
+
+Solution 1 - try every element
+    1. traverse through the whole array nested loop
 */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    //solution 1 - try per element
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i != j && nums[i] == nums[j] && Math.abs(i - j) <= k) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
