1cab81ae64ffd4eb04dde1437f402af2e09adb62
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-07-31 22:11:03 +0800

    add solution 1

--- LC-228-Summary-Ranges.js
+++ LC-228-Summary-Ranges.js
@@ -12,4 +12,41 @@ Return the smallest sorted list of ranges that cover all the numbers in the arra
 Each range [a,b] in the list should be output as:
     "a->b" if a != b
     "a" if a == b
+
+Solution 1 - uses the idea of n - n-1 = 1
+    1. compare current element from previous through subtraction
+    2. if difference = 1, still in range
+    3. else not in range
 */
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+    //sorted
+    //no duplicates
+    //inclusive
+    //includes 0
+    //returns array
+    //elements are string
+
+    //Solution 1 -> n+1 - n = 1
+    let container = [],
+        start = nums[0],
+        counter = 1;
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (nums[i] - nums[i - 1] == 1) {
+            counter++;
+        } else {
+            if (counter == 1) {
+                container.push(`${start}`);
+            } else {
+                container.push(`${start}->${nums[i - 1]}`);
+            }
+            start = nums[i];
+            counter = 1;
+        }
+    }
+    return container;
+};
