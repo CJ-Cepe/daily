86e7f51f3e147078bd131601684fc358d2d0c364
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-08-10 14:10:49 +0800

    add solution 1 steps

--- LC-350-Intersection-of-Two-Arrays-II.js
+++ LC-350-Intersection-of-Two-Arrays-II.js
@@ -15,4 +15,11 @@ Example 2:
     Output: [4,9]
     Explanation: [9,4] is also accepted.
 
+
+Solution 1 -- using objects
+    1. initialize an empty array and object
+    2. traverse through the first array and list each unique item to the object
+    3. traverse through the second array and compare the current element to the content of the object
+    4. if the current element exist in the object, decrement and push element to the result array
+    5. return result array
 */
