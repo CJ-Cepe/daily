381e4a6459cfa4107975cca31464c92a5f9c5f55
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-08-17 17:42:02 +0800

    add solution 1

--- LC-500-Keyboard-Row.js
+++ LC-500-Keyboard-Row.js
@@ -29,3 +29,50 @@ Solution 1
 3. if not, reject word, else push it to the result
 
 */
+
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function (words) {
+    //solution 1 - brute force
+    //per word, check if where is the first element is any of the rows
+    //if it is, check the following letters if it is
+    //push to result if it is
+    //return result
+
+    let row1 = 'qwertyuiop',
+        row2 = 'asdfghjkl',
+        row3 = 'zxcvbnm',
+        checkRow = '',
+        result = [];
+
+    words.forEach((word) => {
+        let original = word;
+        word = word.toLowerCase();
+
+        if (row1.includes(word[0])) {
+            checkRow = row1;
+        } else if (row2.includes(word[0])) {
+            checkRow = row2;
+        } else if (row3.includes(word[0])) {
+            checkRow = row3;
+        }
+
+        let lettersInRow = checkWord(checkRow, word);
+        if (lettersInRow) {
+            result.push(original);
+        }
+    });
+
+    return result;
+};
+
+function checkWord(row, word) {
+    for (let i = 0; i < word.length; i++) {
+        if (!row.includes(word[i])) {
+            return false;
+        }
+    }
+    return true;
+}
