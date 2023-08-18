50b4f960307b53bea97cb585d8c107851a071ce0
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-08-18 11:51:51 +0800

    add solution 2

--- LC-500-Keyboard-Row.js
+++ LC-500-Keyboard-Row.js
@@ -69,6 +69,26 @@ var findWords = function (words) {
     });
 
     return result;
+
+    //solution 2 - utilize set
+    const rows = [
+        new Set('qwertyuiop'),
+        new Set('asdfghjkl'),
+        new Set('zxcvbnm'),
+    ];
+
+    const result = [];
+    for (const word of words) {
+        const chars = word.toLowerCase().split('');
+        for (const row of rows) {
+            if (chars.every(row.has, row)) {
+                result.push(word);
+                break;
+            }
+        }
+    }
+
+    return result;
 };
 
 function checkWord(row, word) {
