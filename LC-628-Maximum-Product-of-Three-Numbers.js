0338c42012b55e27c83b6c9d1732456dbb89160d
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-08-21 22:37:14 +0800

    add solution 1 notes

--- LC-628-Maximum-Product-of-Three-Numbers.js
+++ LC-628-Maximum-Product-of-Three-Numbers.js
@@ -16,5 +16,11 @@ Example 3:
     Input: nums = [-1,-2,-3]
     Output: -6
 
-
+Solution 1
+    sort array - the largest in the last 3, while the smallest in the first 3
+    multiply the last 3
+    multiply the first 3
+    multiply the last 2 and the first 1
+    multiply the first 2 and the last 1
+    get the max of these
 */
