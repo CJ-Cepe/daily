82af75cbbf2150411db0188061ed7676fd351ea8
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-08-26 22:15:16 +0800

    add solution 1 notes

--- LC-575-Distribute-Candies.js
+++ LC-575-Distribute-Candies.js
@@ -11,4 +11,11 @@ The doctor advised Alice to only eat n / 2 of the candies she has (n is always e
 Given the integer array candyType of length n, return the maximum number of different 
     types of candies she can eat if she only eats n / 2 of them.
 
+Solution 1
+    1. change array to set
+    2. compute for the allowed candies using the array.length / 2
+    3. compare if allowed candies >= set size.
+    6. if yes, return allowedCandies
+    7. else return set size
+
 */
